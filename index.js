5daf5298d605602c26b1c198bb450bcac59e24a5
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-08-21 21:59:57 +0500

    add network routes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
+const network = require("./routes/network");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/network", network);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
