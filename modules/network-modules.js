87ba2827003a12d6f76fe175f675c49057d44562
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-09-04 16:20:40 +0500

    change network schema

--- modules/network-modules.js
+++ modules/network-modules.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const networkSchema = new mongoose.Schema({
   name: String,
-  image: String,
+  image: {
+    medium: String,
+    original: String,
+  },
 });
 
 const Network = mongoose.model("Network", networkSchema);
